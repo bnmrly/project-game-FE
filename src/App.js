68185a697a07d0182e7c0ab52530ed37f7e71ec4
luke101308 <lukebirkett113@hotmail.com>
2018-07-10 12:49:51 +0100

    conditionally render login/game

--- src/App.js
+++ src/App.js
@@ -5,26 +5,31 @@ import Wallet from './components/Wallet/Wallet';
 import Bank from './components/Bank/Bank';
 import Card from './components/Card/Card'
 import Login from "./components/Login/Login"
+import store from "./redux/index"
+import {connect} from 'react-redux'
 
-
+// {...store, id : "id"}
 // Conditionaly render login screen or below
 // show loggeddn user name
-
 class App extends Component {
   render() {
     return (<React.Fragment>
-      <Login/>
+      {!this.props.id ? <Login/> :
       <div className="app__game-container">
-       {/* <section className="app__turncounter">turn count goes here</section>
+       <section className="app__turncounter">turn count goes here</section>
        <Dialogue />
         <Wallet />
-         <Bank /> */}
-      </div>
+         <Bank />
+      </div>}
       </React.Fragment>
     );
   }
 }
 
+const mapStateToProps = store => {
+  return {
+    id  :store.id
+  }
+}
+export default connect(mapStateToProps, null)(App);
 
-
-export default App;
