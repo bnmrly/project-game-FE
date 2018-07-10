c7353f0330e118554902beda79ac63f0a36941ff
Ben Marley <benjaminjohnmarley@gmail.com>
2018-07-10 17:52:14 +0100

    change dialogue to display

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import Dialogue from './components/Dialogue/Dialogue';
+import Display from './components/Display/Display';
 import Wallet from './components/Wallet/Wallet';
 import Bank from './components/Bank/Bank';
-import Card from './components/Card/Card'
-import Login from "./components/Login/Login"
-import store from "./redux/index"
-import {connect} from 'react-redux'
+// import Card from './components/Card/Card';
+import Login from './components/Login/Login';
+import store from './redux/index';
+import { connect } from 'react-redux';
 
 // {...store, id : "id"}
 
@@ -16,18 +16,21 @@ import {connect} from 'react-redux'
 // exit bank link. which will take you back to the story object
 
 class App extends Component {
-  store = store.getState().id
-  
+  store = store.getState().id;
+
   render() {
-    console.log(this.props)
-    return (<React.Fragment>
-      {!this.props.id ? <Login/> :
-      <div className="app__game-container">
-       <section className="app__turncounter">turn count goes here</section>
-       <Dialogue />
-        <Wallet />
-         <Bank />
-      </div>}
+    return (
+      <React.Fragment>
+        {!this.props.id ? (
+          <Login />
+        ) : (
+          <div className="app__game-container">
+            <section className="app__turncounter">turn count goes here</section>
+            <Display />
+            <Wallet />
+            <Bank />
+          </div>
+        )}
       </React.Fragment>
     );
   }
@@ -35,8 +38,11 @@ class App extends Component {
 
 const mapStateToProps = store => {
   return {
-    id  :store.id
-  }
-}
-export default connect(mapStateToProps, null)(App);
+    id: store.id
+  };
+};
 
+export default connect(
+  mapStateToProps,
+  null
+)(App);
