81ca95fca1229571619570aeeec074f9c197f1fe
Ben Marley <benjaminjohnmarley@gmail.com>
2018-07-10 17:52:27 +0100

    remove unused function

--- src/redux/reducers/cardSelection.js
+++ src/redux/reducers/cardSelection.js
@@ -3,23 +3,15 @@ const initialState = { cash: 0, credit: 0, rating: 0 };
 const cardSelection = (state = initialState, action) => {
   const payload = action.payload;
   switch (action.type) {
-    case "LOW":
+    case 'LOW':
       return { ...state, rating: payload };
-    case "MEDIUM":
+    case 'MEDIUM':
       return { ...state, rating: payload };
-    case "HIGH":
+    case 'HIGH':
       return { ...state, rating: payload };
     default:
       return state;
   }
 };
 
-
-
-const mapStateToProps = state => {
-  return {
-    wallet: cardSelection(state, state.cardSelectionEvent)
-  }
-}
-
-export default cardSelection
+export default cardSelection;
