f66452c9f090b881b51ab8a5a82c615ea02a054a
Ben Marley <benjaminjohnmarley@gmail.com>
2018-07-11 17:45:32 +0100

    fix merge conflict

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,41 +1,6 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
-import { connect } from "react-redux";
-import { idSetterEvent } from '../../redux/actions/PlayerInfoAction';
-class Login extends Component {
-  state = {
-    idInput: ''
-  }
-  render() {
-    return (
-      <React.Fragment>
-        <p>If you are working solo, please use id zzzzz</p>
-        <input placeholder="please enter your group id" value={this.state.idInput} onChange={this.handleChange} />
-        {/* if input length isn't 5 throw error? */}
-        <button type="submit" onClick={() => this.props.handleClick(this.state.idInput)}>Login</button>
-      </React.Fragment>
-    );
-  }
-  handleChange = (e) => {
-    this.setState({ idInput: e.target.value })
-  }
-}
-const mapDispatchToProps = dispatch => {
-  return {
-    handleClick: (id) => {
-      dispatch(idSetterEvent(id))
-    }
-  }
-}
-const mapStateToProps = store => {
-  return {
-    id: store.playerInfo.id
-  }
-}
-export default connect(mapStateToProps, mapDispatchToProps)(Login);
-=======
 import { connect } from 'react-redux';
-import idSelectionEvent from '../../redux/actions/idSelectionEvent';
+import { idSetterEvent } from '../../redux/actions/PlayerInfoAction';
 class Login extends Component {
   state = {
     idInput: ''
@@ -66,17 +31,16 @@ class Login extends Component {
 const mapDispatchToProps = dispatch => {
   return {
     handleClick: id => {
-      dispatch(idSelectionEvent(id));
+      dispatch(idSetterEvent(id));
     }
   };
 };
 const mapStateToProps = store => {
   return {
-    id: store.id
+    id: store.playerInfo.id
   };
 };
 export default connect(
   mapStateToProps,
   mapDispatchToProps
 )(Login);
->>>>>>> dev
