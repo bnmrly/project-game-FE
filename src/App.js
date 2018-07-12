6d87866c59e34825b29c3fb481dae1a1528aa32e
luke101308 <lukebirkett113@hotmail.com>
2018-07-12 10:08:44 +0100

    remove console.log

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import { connect } from 'react-redux';
 
 class App extends Component {
   render() {
-    console.log(store.getState());
     return (
       <React.Fragment>
         {!this.props.id ? (
