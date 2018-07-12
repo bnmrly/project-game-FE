72f12e89f5cb4c9c6fd5107049bb93cd7d5363ba
Renaldas <renaldasjakas@gmail.com>
2018-07-12 09:47:31 +0100

    added circular progress bar for displaying ratings health and display credit available, added text showing total cash

--- src/components/Wallet/Wallet.js
+++ src/components/Wallet/Wallet.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
 import './Wallet.css';
+import { connect } from 'react-redux';
+import CircularProgressbar from 'react-circular-progressbar';
 class Wallet extends Component {
   render() {
     return (
       <section className="wallet__container">
-        <div></div>
-      </section>
+        <div style={{
+          width: "150px"
+        }}>
+          <CircularProgressbar className="rating"
+            percentage={this.props.wallet.rating / 10}
+            text={"Rating"} />
+          <CircularProgressbar className="credit"
+            percentage={(this.props.wallet.credit.available / this.props.wallet.credit.max) * 100}
+            text={`£${this.props.wallet.credit.available}/£${this.props.wallet.credit.max}`} />
+          <b> Cash : £{this.props.wallet.cash}</b>
+        </div>
+      </section >
     );
   }
 }
-
-export default Wallet;
+const mapStateToProps = store => {
+  return {
+    wallet: store.playerInfo.wallet
+  }
+}
+export default connect(mapStateToProps, null)(Wallet)
