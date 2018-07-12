0b8b8394aa5c5ec3ba8ed1e175d1852bce2ddf1e
Ben Marley <benjaminjohnmarley@gmail.com>
2018-07-12 12:17:58 +0100

    linting

--- src/components/Display/Display.js
+++ src/components/Display/Display.js
@@ -2,11 +2,8 @@ import React, { Component } from 'react';
 import './Display.css';
 import data from '../../data/gameplay.json';
 import Card from '../Card/Card';
+import Name from '../Name/Name';
 import shortId from 'short-id';
-
-// to avoid setting state in render
-// chnage if block to create next chapter button
-// let that button change state
 
 // TO IMPLEMENT:
 
@@ -27,38 +24,40 @@ class Display extends Component {
   storyRevealer = () => {
     const storyLines = [];
     const { storyBook, turnCount } = this.state;
-      for (let i = 0; i < turnCount; i++) {
-        storyLines.unshift(<p key={shortId.generate()}>{storyBook[i].text}</p>);
-      }
-      const buttons = (
-        <div key={shortId.generate()}>
-          {storyBook[turnCount - 1].choices ? (
-            storyBook[turnCount - 1].choices.forEach(choice => {
-              switch (choice) {
-                case 'Card':
-                  storyLines.unshift(
-                    <div
-                      key={shortId.generate()}
-                      onClick={this.nextClickHandler}
-                    >
-                      <Card />
-                    </div>
-                  );
-                  break;
-                default:
-                  console.log('dummy text');
-              }
-            })
-          ) : (
-            turnCount === storyBook.length ?
-            <button onClick={this.nextChapterClickHandler}>next chapter</button>
-            : <button onClick={this.nextClickHandler}>next</button>
-          )}
-        </div>
-      );
-      storyLines.unshift(buttons);
-      return storyLines;
-
+    for (let i = 0; i < turnCount; i++) {
+      storyLines.unshift(<p key={shortId.generate()}>{storyBook[i].text}</p>);
+    }
+    const buttons = (
+      <div key={shortId.generate()}>
+        {storyBook[turnCount - 1].choices ? (
+          storyBook[turnCount - 1].choices.forEach(choice => {
+            switch (choice) {
+              case 'Card':
+                storyLines.unshift(
+                  <div key={shortId.generate()} onClick={this.nextClickHandler}>
+                    <Card />
+                  </div>
+                );
+                break;
+              case 'Name':
+                storyLines.unshift(
+                  // <div key={shortId.generate()} onClick={this.nextClickHandler}>
+                  <Name />
+                  // </div>
+                );
+              default:
+                console.log('dummy text');
+            }
+          })
+        ) : turnCount === storyBook.length ? (
+          <button onClick={this.nextChapterClickHandler}>next chapter</button>
+        ) : (
+          <button onClick={this.nextClickHandler}>next</button>
+        )}
+      </div>
+    );
+    storyLines.unshift(buttons);
+    return storyLines;
   };
 
   nextClickHandler = () => {
@@ -83,9 +82,7 @@ class Display extends Component {
         chapterCount: this.state.chapterCount + 1
       });
     }
-  }
+  };
 }
-
-
 
 export default Display;
