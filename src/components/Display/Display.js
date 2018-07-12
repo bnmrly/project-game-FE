8901b7ca9b64f3ea014a61bdb1692a224e0f1a85
luke101308 <lukebirkett113@hotmail.com>
2018-07-12 10:08:01 +0100

    add new chapter button to prevent re-render within render

--- src/components/Display/Display.js
+++ src/components/Display/Display.js
@@ -27,24 +27,6 @@ class Display extends Component {
   storyRevealer = () => {
     const storyLines = [];
     const { storyBook, turnCount } = this.state;
-    if (turnCount > storyBook.length) {
-      if (this.state.chapterCount === 3) {
-        this.setState({
-          storyBook: data.fixedChapters.finale,
-          turnCount: 1,
-          chapterCount: 4
-        });
-      } else {
-        this.setState({
-          storyBook:
-            data.storyBoard[
-              Object.keys(data.storyBoard)[Math.floor(Math.random() * 4)]
-            ],
-          turnCount: 1,
-          chapterCount: this.state.chapterCount + 1
-        });
-      }
-    } else {
       for (let i = 0; i < turnCount; i++) {
         storyLines.unshift(<p key={shortId.generate()}>{storyBook[i].text}</p>);
       }
@@ -68,13 +50,15 @@ class Display extends Component {
               }
             })
           ) : (
-            <button onClick={this.nextClickHandler}>next</button>
+            turnCount === storyBook.length ?
+            <button onClick={this.nextChapterClickHandler}>next chapter</button>
+            : <button onClick={this.nextClickHandler}>next</button>
           )}
         </div>
       );
       storyLines.unshift(buttons);
       return storyLines;
-    }
+
   };
 
   nextClickHandler = () => {
@@ -82,6 +66,26 @@ class Display extends Component {
       turnCount: this.state.turnCount + 1
     });
   };
+  nextChapterClickHandler = () => {
+    if (this.state.chapterCount === 3) {
+      this.setState({
+        storyBook: data.fixedChapters.finale,
+        turnCount: 1,
+        chapterCount: 4
+      });
+    } else {
+      this.setState({
+        storyBook:
+          data.storyBoard[
+            Object.keys(data.storyBoard)[Math.floor(Math.random() * 4)]
+          ],
+        turnCount: 1,
+        chapterCount: this.state.chapterCount + 1
+      });
+    }
+  }
 }
+
+
 
 export default Display;
