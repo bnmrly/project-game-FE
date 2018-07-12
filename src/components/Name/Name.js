95279a9660f6379ccaaa24788a5e6d979b796557
Ben Marley <benjaminjohnmarley@gmail.com>
2018-07-12 12:18:23 +0100

    create name component

---
+++ src/components/Name/Name.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import './Name.css';
+import { connect } from 'react-redux';
+import { nameSetterEvent } from '../../redux/actions/PlayerInfoAction';
+
+class Name extends Component {
+  state = {
+    name: ''
+  };
+  render() {
+    return (
+      <section className="name">
+        <input
+          required="true"
+          placeholder="Please enter your name"
+          value={this.state.name}
+          onChange={this.handleChange}
+        />
+        <button value={this.state.name} onClick={this.props.handleClick}>
+          Submit
+        </button>
+      </section>
+    );
+  }
+  handleChange = e => {
+    this.setState({ name: e.target.value });
+  };
+}
+const mapDispatchToProps = dispatch => {
+  return {
+    handleClick: e => {
+      dispatch(nameSetterEvent(e.target.value));
+    }
+  };
+};
+const mapStateToProps = store => {
+  return {
+    name: store.playerInfo.name
+  };
+};
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Name);
