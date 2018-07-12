561e26beaed6f5687315ede2fe0c637846384e77
Renaldas <renaldasjakas@gmail.com>
2018-07-12 14:32:36 +0100

    implement turn count with redux

--- src/components/Display/Display.js
+++ src/components/Display/Display.js
@@ -1,8 +1,13 @@
-import React, { Component } from 'react';
-import './Display.css';
-import data from '../../data/gameplay.json';
-import Card from '../Card/Card';
-import shortId from 'short-id';
+import React, { Component } from "react";
+import "./Display.css";
+import data from "../../data/gameplay.json";
+import Card from "../Card/Card";
+import shortId from "short-id";
+import { connect } from "react-redux";
+import {
+  increaseTurnCount,
+  resetTurnCount
+} from "../../redux/actions/PlayerInfoAction";
 
 // to avoid setting state in render
 // chnage if block to create next chapter button
@@ -15,7 +20,6 @@ import shortId from 'short-id';
 class Display extends Component {
   state = {
     storyBook: data.fixedChapters.intro,
-    turnCount: 1,
     chapterCount: 0
   };
   render() {
@@ -26,66 +30,68 @@ class Display extends Component {
 
   storyRevealer = () => {
     const storyLines = [];
-    const { storyBook, turnCount } = this.state;
-      for (let i = 0; i < turnCount; i++) {
-        storyLines.unshift(<p key={shortId.generate()}>{storyBook[i].text}</p>);
-      }
-      const buttons = (
-        <div key={shortId.generate()}>
-          {storyBook[turnCount - 1].choices ? (
-            storyBook[turnCount - 1].choices.forEach(choice => {
-              switch (choice) {
-                case 'Card':
-                  storyLines.unshift(
-                    <div
-                      key={shortId.generate()}
-                      onClick={this.nextClickHandler}
-                    >
-                      <Card />
-                    </div>
-                  );
-                  break;
-                default:
-                  console.log('dummy text');
-              }
-            })
-          ) : (
-            turnCount === storyBook.length ?
-            <button onClick={this.nextChapterClickHandler}>next chapter</button>
-            : <button onClick={this.nextClickHandler}>next</button>
-          )}
-        </div>
-      );
-      storyLines.unshift(buttons);
-      return storyLines;
-
+    const { storyBook } = this.state;
+    for (let i = 0; i < this.props.turnCount; i++) {
+      storyLines.unshift(<p key={shortId.generate()}>{storyBook[i].text}</p>);
+    }
+    const buttons = (
+      <div key={shortId.generate()}>
+        {storyBook[this.props.turnCount - 1].choices ? (
+          storyBook[this.props.turnCount - 1].choices.forEach(choice => {
+            switch (choice) {
+              case "Card":
+                storyLines.unshift(<Card />);
+                break;
+              default:
+                console.log("dummy text");
+            }
+          })
+        ) : this.props.turnCount === storyBook.length ? (
+          <button onClick={this.nextChapterClickHandler}>next chapter</button>
+        ) : (
+          <button onClick={this.props.turnIncrement}>next</button>
+        )}
+      </div>
+    );
+    storyLines.unshift(buttons);
+    return storyLines;
   };
 
-  nextClickHandler = () => {
-    this.setState({
-      turnCount: this.state.turnCount + 1
-    });
-  };
   nextChapterClickHandler = () => {
     if (this.state.chapterCount === 3) {
+      this.props.turnReset();
       this.setState({
         storyBook: data.fixedChapters.finale,
-        turnCount: 1,
         chapterCount: 4
       });
     } else {
+      this.props.turnReset();
       this.setState({
         storyBook:
           data.storyBoard[
             Object.keys(data.storyBoard)[Math.floor(Math.random() * 4)]
           ],
-        turnCount: 1,
         chapterCount: this.state.chapterCount + 1
       });
     }
-  }
+  };
 }
-
-
-
-export default Display;
+const mapDispatchToProps = dispatch => {
+  return {
+    turnIncrement: () => {
+      dispatch(increaseTurnCount());
+    },
+    turnReset: () => {
+      dispatch(resetTurnCount());
+    }
+  };
+};
+const mapStateToProps = store => {
+  return {
+    turnCount: store.gameProgress.turn_count
+  };
+};
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Display);
--- src/redux/actions/PlayerInfoAction.js
+++ src/redux/actions/PlayerInfoAction.js
@@ -1,24 +1,34 @@
 export function idSetterEvent(id) {
-    return {
-        type: 'SET_ID',
-        payload: id
-    }
-};
+  return {
+    type: "SET_ID",
+    payload: id
+  };
+}
 export function usernameSetterEvent(username) {
-    return {
-        type: 'SET_USERNAME',
-        payload: username
-    }
-};
+  return {
+    type: "SET_USERNAME",
+    payload: username
+  };
+}
 export function cardSelectionEvent(data) {
-    return {
-        type: data,
-        payload: data === "LOW" ? 1 : data === "MEDIUM" ? 2 : 3
-    };
-};
+  return {
+    type: data,
+    payload: data === "LOW" ? 1 : data === "MEDIUM" ? 2 : 3
+  };
+}
 export function setUserWage(wage) {
-    return {
-        type: "SET_USER_WAGE",
-        payload: wage
-    }
-};
+  return {
+    type: "SET_USER_WAGE",
+    payload: wage
+  };
+}
+export function increaseTurnCount() {
+  return {
+    type: "INC_TURNCOUNT"
+  };
+}
+export function resetTurnCount() {
+  return {
+    type: "TURN_RESET"
+  };
+}
