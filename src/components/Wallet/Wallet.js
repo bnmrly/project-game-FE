40bcf987dd39b3649d27fa35c80f3751d2099744
Ben Marley <benjaminjohnmarley@gmail.com>
2018-07-16 15:56:51 +0100

    wallet layout

--- src/components/Wallet/Wallet.js
+++ src/components/Wallet/Wallet.js
@@ -6,25 +6,46 @@ class Wallet extends Component {
   render() {
     return (
       <section className="wallet__container">
-        <div style={{
-          width: "150px"
-        }}>
-          <CircularProgressbar className="rating"
+        <div
+          style={{
+            width: '150px'
+          }}
+        >
+          <p className="p-progress">Credit Rating</p>
+          <CircularProgressbar
+            className="rating"
             percentage={this.props.wallet.rating / 10}
-            text={"Rating"} />
-          <CircularProgressbar className="credit"
-            percentage={(this.props.wallet.credit.available / this.props.wallet.credit.max) * 100}
-            text={`£${this.props.wallet.credit.available}/£${this.props.wallet.credit.max}`} />
-          <b> Cash : £{this.props.wallet.cash}</b>
+            text={this.props.wallet.rating}
+          />
+          <p className="p-progress">Available Credit / Max Credit</p>
+          <CircularProgressbar
+            className="credit"
+            percentage={
+              (this.props.wallet.credit.available /
+                this.props.wallet.credit.max) *
+              100
+            }
+            text={`£${this.props.wallet.credit.available}/£${
+              this.props.wallet.credit.max
+            }`}
+          />
+          <p className="p-progress">Cash</p>
+          <CircularProgressbar
+            className="cash"
+            text={`£${this.props.wallet.cash}`}
+            percentage={100}
+          />
         </div>
-      </section >
+      </section>
     );
   }
 }
 const mapStateToProps = store => {
   return {
     wallet: store.playerFinancialInfo.wallet
-  }
-}
-export default connect(mapStateToProps, null)(Wallet)
-
+  };
+};
+export default connect(
+  mapStateToProps,
+  null
+)(Wallet);
