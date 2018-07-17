fb8e13130ff282daa96937814af76e69a7b9db1f
hannahbennett27 <hannah.bennett27@hotmail.co.uk>
2018-07-17 11:38:55 +0100

    update new player initial dataset

--- src/firebase/fb.js
+++ src/firebase/fb.js
@@ -1,29 +1,37 @@
 import { db } from './config';
 
 export const initialisePlayer = (name, id) => {
-    const addPlayers = db
-        .collection('games')
-        .doc(id);
+  const addPlayers = db.collection('games').doc(id);
 
-    addPlayers.get()
-        .then(docSnapShot => {
-            console.log(docSnapShot)
-            if (docSnapShot.exists) {
-                const setWithMerge = addPlayers.set({
-                    players: {
-                        [name]: {
-                            rating: 0,
-                            creditSpent: 0,
-                            creditMax: 0,
-                            cash: 0,
-                            decisions: {
-                                testDecision: 'test'
-                            }
-                        }
-                    }
-                }, { merge: true });
-
-                return setWithMerge;
+  addPlayers.get().then(docSnapShot => {
+    console.log(docSnapShot);
+    if (docSnapShot.exists) {
+      const setWithMerge = addPlayers.set(
+        {
+          players: {
+            [name]: {
+              rating: [0], // progress through chapters
+              creditAvail: [0], // progress through chapters
+              cashAvail: [0], // progress through chapters
+              chapterCount: 0,
+              billPostpones: 0, // count
+              cashSpends: 0, // count
+              cardSpends: 0, // count
+              result: null,
+              decisions: {
+                card: null, // low/med/high
+                phone: null, // high/sim-only/second-hand
+                clothing: null, // shopping choice
+                night: null, // eve entertainment choice
+                careerProgression: null // boolean - career progression (true) vs holiday (false)
+              }
             }
-        })
+          }
+        },
+        { merge: true }
+      );
+
+      return setWithMerge;
+    }
+  });
 };
