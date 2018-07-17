50cc408f1bb733d552badf2729df4c8f154c7919
tricky93 <ptmwalsh1@gmail.com>
2018-07-17 13:50:54 +0100

    add modal to display hints

--- src/components/Snippets/Snippets.js
+++ src/components/Snippets/Snippets.js
@@ -1,29 +1,56 @@
-import React, { Component } from 'react';
-import data from '../../data/gameplay.json';
-import { connect } from 'react-redux';
-import './Snippets.css';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import Modal from "react-modal";
+import data from "../../data/gameplay.json";
+import { connect } from "react-redux";
+import "./Snippets.css";
 
 class Snippets extends Component {
   state = {
-    displayedSnippets: []
+    displayedSnippets: [],
+    modalIsOpen: false
   };
   render() {
+    const snippets = this.state.displayedSnippets;
     return (
       <React.Fragment>
-        <button className="hint" onClick={this.addSnippet}>
-          New Hint
+        <button className="hint" onClick={this.openModal}>
+          Hint
         </button>
-        <div className="container__snippets">
-          {this.state.displayedSnippets
-            .slice()
-            .reverse()
-            .map(snippet => {
-              return <p>{snippet}</p>;
-            })}
+        <div>
+          <Modal
+            snippets={snippets}
+            isOpen={this.state.modalIsOpen}
+            onAfterOpen={this.afterOpenModal}
+            onRequestClose={this.closeModal}
+            className="content w3-animate-opacity"
+          >
+            <h3 className="close-button" onClick={this.closeModal}>
+              X
+            </h3>
+            <div className="container__snippets">
+              {snippets
+                .slice()
+                .reverse()
+                .map(snippet => {
+                  return <p className="modal-content">{snippet}</p>;
+                })}
+            </div>
+          </Modal>
         </div>
       </React.Fragment>
     );
   }
+
+  openModal = () => {
+    this.addSnippet();
+    this.setState({ modalIsOpen: true });
+  };
+
+  closeModal = () => {
+    this.setState({ modalIsOpen: false });
+  };
+
   addSnippet = () => {
     if (this.state.displayedSnippets.length > 0) {
       const newSnippets = [...this.state.displayedSnippets];
